4f98cbcb5942791bd190e54a503abd5980412a73
Taras KOvtun <taraskovtun@gmail.com>
2023-10-06 10:48:51 -0700

    added routes

--- online-learning/src/app/app-routing.module.ts
+++ online-learning/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'courses/:id', component: CourseDetailComponent },
+  { path: 'profile', component: UserProfileComponent },
+  { path: 'admin', component: AdminComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- online-learning/src/app/app.module.ts
+++ online-learning/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseDetailComponent } from './course-detail/course-detail.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    CourseDetailComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
